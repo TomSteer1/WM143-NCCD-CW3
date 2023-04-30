0a42f3c5ea0badaec285aa6060d5e924e43d5541
Tom <tom@tomsteer.me>
2023-04-30 22:28:39 +0100

    Automatic Testing

--- testMachines.sh
+++ testMachines.sh
@@ -2,14 +2,16 @@
 echo "Removing previous tests" 1>&2
 rm _test/*.txt 2>/dev/null
 echo "Starting machines" 1>&2
-lstart --tmux-detached
+#lstart --tmux-detached
 echo "Machines started" 1>&2
 echo "Getting list of machines" 1>&2
 machines=$(vconnect -l | awk '{print $1}')
 for machine in $machines; do
-	# Rempve colon from machine name
+	# Remove colon from machine name
 	machine=${machine%:}
 	echo "Testing $machine" 1>&2
+	# Runs a dump command to ensure the script runs properly
+	vcommand $machine "id" > /dev/null
 	vcommand $machine "/root/test.sh" > /dev/null
 done
 echo "All machines tested" 1>&2
@@ -19,11 +21,111 @@ while [ $(ls _test | wc -l) -ne $(vconnect -l | wc -l) ]; do
 	sleep 1
 done
 echo "Stopping machines" 1>&2
-lcrash
+#lcrash
 echo "Machines stopped" 1>&2
-echo "Results are:" 1>&2
+#echo "Results are:" 1>&2
+#for machine in $machines; do
+	# Remove colon from machine name
+#	machine=${machine%:}
+#	echo "$machine: $(cat _test/$machine.txt)" 1>&2
+#done
+echo "Parsing results" 1>&2
+# Parse Results into arrays of machine names each represented by a bit in the result
+ExtWWW=( ) # External Web Server
+ExtDNS=( ) # External DNS Server
+IntWWW=( ) # Internal Web Server
+Mail=( ) # Mail Server
+LDAP=( ) # LDAP Server
+IntDNS=( ) # Internal DNS Server
+Squid=( ) # Squid Proxy
+WebDB=( ) # Web Database
+MailDB=( ) # Mail Database
+HrDB=( ) # HR Database
 for machine in $machines; do
-	# Rempve colon from machine name
 	machine=${machine%:}
-	echo "$machine: $(cat _test/$machine.txt)" 1>&2
+	result=$(cat _test/$machine.txt)
+	if [ ${result:0:1} -eq 1 ]; then
+		ExtWWW+=($machine)
+	fi
+	if [ ${result:1:1} -eq 1 ]; then
+		ExtDNS+=($machine)
+	fi
+	if [ ${result:2:1} -eq 1 ]; then
+		IntWWW+=($machine)
+	fi
+	if [ ${result:3:1} -eq 1 ]; then
+		Mail+=($machine)
+	fi
+	if [ ${result:4:1} -eq 1 ]; then
+		LDAP+=($machine)
+	fi
+	if [ ${result:5:1} -eq 1 ]; then
+		IntDNS+=($machine)
+	fi
+	if [ ${result:6:1} -eq 1 ]; then
+		Squid+=($machine)
+	fi
+	if [ ${result:7:1} -eq 1 ]; then
+		WebDB+=($machine)
+	fi
+	if [ ${result:8:1} -eq 1 ]; then
+		MailDB+=($machine)
+	fi
+	if [ ${result:9:1} -eq 1 ]; then
+		HrDB+=($machine)
+	fi
 done
+# Prints what machines can be accessed by
+# Checks if length of array is equal to number of machines
+echo ${#ExtWWW[@]}
+if [ ${#ExtWWW[@]} -eq $count ]; then
+	echo "External Web Server can be accessed by all machines"
+else
+	echo "External Web Server can be accessed by ${ExtWWW[@]}"
+fi
+if [ ${#ExtDNS[@]} -eq $count ]; then
+	echo "External DNS Server can be accessed by all machines"
+else
+	echo "External DNS Server can be accessed by ${ExtDNS[@]}"
+fi
+if [ ${#IntWWW[@]} -eq $count ]; then
+	echo "Internal Web Server can be accessed by all machines"
+else
+	echo "Internal Web Server can be accessed by ${IntWWW[@]}"
+fi
+if [ ${#Mail[@]} -eq $count ]; then
+	echo "Mail Server can be accessed by all machines"
+else
+	echo "Mail Server can be accessed by ${Mail[@]}"
+fi
+if [ ${#LDAP[@]} -eq $count ]; then
+	echo "LDAP Server can be accessed by all machines"
+else
+	echo "LDAP Server can be accessed by ${LDAP[@]}"
+fi
+if [ ${#IntDNS[@]} -eq $count ]; then
+	echo "Internal DNS Server can be accessed by all machines"
+else
+	echo "Internal DNS Server can be accessed by ${IntDNS[@]}"
+fi
+if [ ${#Squid[@]} -eq $count ]; then
+	echo "Squid Proxy can be accessed by all machines"
+else
+	echo "Squid Proxy can be accessed by ${Squid[@]}"
+fi
+if [ ${#WebDB[@]} -eq $count ]; then
+	echo "Web Database can be accessed by all machines"
+else
+	echo "Web Database can be accessed by ${WebDB[@]}"
+fi
+if [ ${#MailDB[@]} -eq $count ]; then
+	echo "Mail Database can be accessed by all machines"
+else
+	echo "Mail Database can be accessed by ${MailDB[@]}"
+fi
+if [ ${#HrDB[@]} -eq $count ]; then
+	echo "HR Database can be accessed by all machines"
+else
+	echo "HR Database can be accessed by ${HrDB[@]}"
+fi
+echo "Done" 1>&2
